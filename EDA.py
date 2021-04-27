db9b57956ae8636dd75b36af0b8328f109e0b6ad
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-27 17:40:57 -0400

    Loading dataset

--- EDA.py
+++ EDA.py
@@ -5,8 +5,12 @@ import seaborn as sns
 import numpy  as np
 import math
 
-# Loading Dataset
+''' Pre-processing Step '''
+'''1. Loading Dataset'''
 
-data = 
+data = pd.read_csv('DataSet\deliveryDataset.csv', sep = ',')
+print(data.head(5)) #View the first 5 rows 
+print(data.info()) #Display the features's types
+
 
 
