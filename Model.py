4d1192d6a1dcadb3f68e1097267199d27d8acc1d
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-27 22:24:29 -0400

    Correlation

--- Model.py
+++ Model.py
@@ -1,3 +1,47 @@
-from EDA import data, data_corr
+# Import libraries and Data from EDA.py
 
-print(data)
+from EDA import data, data_corr, catg_var, cont_var
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+
+
+# Split the Cleaned Dataset (data)
+# Our label : Reached_YN (0,1) = (Yes, No)
+label_class = data['Reached_YN'].astype('int')
+data_ = data.drop('Reached_YN', axis=1)
+# Before, we need to encoder the categorical variables
+data['Product_importance'] = data['Product_importance'].map(
+    {'low': 1, 'medium': 2, 'high': 3})
+# Encoder model
+encoder = LabelEncoder()
+for col in catg_var[:-1]:
+    data_[col] = encoder.fit_transform(data_[col])
+
+
+# Split the dataset with test_size = 20% and 80% of dataset for train the model
+train_features, test_features, train_label, test_label = train_test_split(
+    data_, label_class, test_size=.2, random_state=42)
+
+# 1. Decision Tree Model
+
+# Define the model
+Tree = DecisionTreeClassifier()
+# Use the GrideSearch for tuning the hyperparameter like max_depth and the choose between 'gini' an 'entropy' criterion
+modelTreeTuned = GridSearchCV(Tree, param_grid={'max_depth': range(
+    4, 13), 'criterion': ['gini', 'entropy']})
+# Train the Decision Tree model (the best : the GridSearch's output)
+modelTreeTuned.fit(train_features, train_label)
+print(modelTreeTuned.best_params_)
+
+# Predictions
+train_pred = modelTreeTuned.predict(train_features)
+test_pred = modelTreeTuned.predict(test_features)
+
+# Matrix Confusion
+print('Classification Report of train_data \n',
+      classification_report(train_label, train_pred))
+print('Classification Report of test_data \n',
+      classification_report(test_label, test_pred))
